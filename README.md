d1f09999417985515bc97c07726a84b701ab364c
Queen <uchezumez@gmail.com>
2022-08-30 07:04:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Weather Project
+
+This solution is from Angela Yu's Udemy Web Development Boot Camp Course - Section 20.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Requirement](#requirement)
+- [Author](#author)
+
+## Overview
+  - Angela introduces nodejs and express in this section.
+  - Implementing API's to project (OpenWeatherMap)  
+  - Retrieving live data from api location, parse and render into website
+
+### Screenshot
+ ![Alt text](public/images/Screenshot.png?raw=true)
+ ![Alt text](public/images/Screenshot2.png?raw=true)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
+- NodeJs
+- Express
+- OpenWeatherMap API
+
+
+### Requirement
+
+- run _npm install_ on terminal
+ - Create an account on [OpenWeatherMap.org](https://home.openweathermap.org/)
+ - Locate and copy your personal apikey
+ - In the root directory create a new file named **apiKeys.js** 
+ - Create const with name **openWeatherMapAPIKey** in new file and assign value of personal apiKey retrieved
+ - export const 
+
+Final result should look like
+>exports.openWeatherMapAPIKey = "6fdf*******e588";  
+
+That's all you'll Need.
+Happy coding!❤💚
+
+
+## Author
+
+- Website - [UmesiQueen](https://umesiqueen.github.io/UmesiQueen/)
+- Twitter - [@UmesiQueen](https://www.twitter.com/UmesiQueen)
+
+
---
+++ app.js
@@ -0,0 +1,74 @@
+const { response } = require("express");
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const apiKeys = require("./apiKeys"); 
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const units = "metric";
+  const apiKey = apiKeys.openWeatherMapAPIKey;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    units;
+
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const name = weatherData.name;
+      const country = weatherData.sys.country;
+      const temp = weatherData.main.temp;
+      const weather = weatherData.weather[0].main;
+      const weatherDesc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.send(
+        " <!DOCTYPE html> <html lang='en'><head><meta charset='UTF-8' />" +
+          "<meta http-equiv='X-UA-Compatible' content='IE=edge' />" +
+          "<meta name='viewport' content='width=device-width, initial-scale=1.0'/>" +
+          "<title>Weather App</title><link rel='stylesheet' href='css/style.css' />" +
+          "</head><body><div class='container main'><h1>Weather App</h1><p class='query subtext'> " +
+          name +
+          " </p><div><ul><li class='text'>Country : " +
+          country +
+          "</li>" +
+          "<li class='text'>Weather : " +
+          weather +
+          " </li>" +
+          "<li class='text'>Weather Description : " +
+          weatherDesc +
+          " </li>" +
+          "<li class='text'>Temperature : " +
+          temp +
+          " Degrees Celsius</li> " +
+          "</ul>" +
+          "<img src='" +
+          imageURL +
+          "' alt='Unavailable'/></div>" +
+          "</div></body></html>"
+      );
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+});
